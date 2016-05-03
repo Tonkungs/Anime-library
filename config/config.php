5b453372c5caa51702df0ecf9752266df2f9dd06
Tonkung <ton_naruto_v.2@hotmail.com>
2016-05-03 18:46:37 +0700

    โปรเจควิชาเว็บเพจ

---
+++ config/config.php
@@ -0,0 +1,25 @@
+﻿<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<?php
+
+	ini_set('display_errors', 1);
+	error_reporting(~0);
+
+   $serverName = "localhost";
+   $userName = "root";
+   $userPassword = "";
+   $dbName = "animery";
+   $DateResultNow=date("Y-m-d H:i:s", mktime(date("H")+6));
+   $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+  //mysqli_query("SET collation_connection = utf8_general_ci");
+   mysqli_query($conn,"SET NAMES UTF8");
+	if (mysqli_connect_errno())
+	{
+		echo "Database Connect Failed : " . mysqli_connect_error();
+	}
+	else
+	{
+		//echo "Database Connected.";
+	}
+	//mysqli_close($conn);
+	$salt = "aB1cD2eF3G";
+?>
